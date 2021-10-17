2d4eb978093cb43081f0b0410750dcd08650145f
pbartko <pbartko@netland.com.pl>
2021-10-17 15:02:26 +0200

    add draw method

--- Zadanie2.3/Form1.cs
+++ Zadanie2.3/Form1.cs
@@ -27,11 +27,8 @@ namespace Zadanie2._3
         {
             Wykres.Series.Clear();
             var series1 = Wykres.Series.Add("łamane");
-            series1.ChartType = SeriesChartType.Spline;
             var series2 = Wykres.Series.Add("punkty");
-            series2.ChartType = SeriesChartType.Point;
             var series3 = Wykres.Series.Add("sinus");
-            series3.ChartType = SeriesChartType.Spline;
 
             var series1XPoints = new List<double> {-2, -1.7, -1.3, -0.8, 0, 0.8, 1.3, 1.7, 2, 1.7, 1.3, 0.8, 0, -0.8, -1.3, -1.7, -2};
             var series1YPoints = new List<double> {0, 0.8, 1.5, 1.8, 2, 1.8, 1.5, 0.8, 0, -0.8, -1.5, -1.8, -2, -1.8, -1.5, -0.8, 0 };
@@ -42,15 +39,16 @@ namespace Zadanie2._3
             var series3XPoints = new List<double> {-1, 0, 1 };
             var series3YPoints = new List<double> {0, -1, 0 };
 
-            for (var i = 0; i < series1XPoints.Count; i++)
-                series1.Points.AddXY(series1XPoints[i], series1YPoints[i]);
+            Draw(series1, series1XPoints, series1YPoints, SeriesChartType.Spline);
+            Draw(series2, series2XPoints, series2YPoints, SeriesChartType.Point);
+            Draw(series3, series3XPoints, series3YPoints, SeriesChartType.Spline);
+        }
 
-            for (var i = 0; i < series2XPoints.Count; i++)
-                series2.Points.AddXY(series2XPoints[i], series2YPoints[i]);
-
-            for (var i = 0; i < series3XPoints.Count; i++)
-                series3.Points.AddXY(series3XPoints[i], series3YPoints[i]);
-
+        private void Draw(Series series, List<double> xValues, List<double> yValues, SeriesChartType chartType)
+        {
+            series.ChartType = chartType;
+            for (var i = 0; i < xValues.Count; i++)
+                series.Points.AddXY(xValues[i], yValues[i]);
         }
     }
 }
