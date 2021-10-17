15dc4ed70260353d130065710f32716e00781d1e
pbartko <pbartko@netland.com.pl>
2021-10-17 14:07:07 +0200

    change name

---
+++ Zadanie1/DecisionSystem.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace ConsoleApp3
+{
+    public class DecisionSystem
+    {
+        public List<List<string>> ReadSamples(string pathSamples, char separator)
+        {
+            Replace(pathSamples);
+            return ReadFile(pathSamples, separator);
+        }
+
+        public List<string> ReadAttributeNames(string pathValues, char separator)
+        {
+            Replace(pathValues);
+            var attrs = ReadFile(pathValues, separator);
+            var names = new List<string>();
+            attrs.ForEach(x => names.Add(x[0]));
+
+            return names;
+        }
+
+        public List<bool> CheckIfAttrSym(string pathValues, char separator)
+        {
+            Replace(pathValues);
+            var attrs = ReadFile(pathValues, separator);
+            var ifAttrSym = new List<bool>();
+            attrs.ForEach(x=>ifAttrSym.Add(x.Last() == "s"));
+
+            return ifAttrSym;
+        }
+
+        private static List<List<string>> ReadFile(string path, char separator)
+        {
+            return File.ReadAllLines(path).Select(x => x.Split(separator).ToList()).ToList();
+        }
+
+        private static void Replace(string path)
+        {
+            var file = File.ReadAllText(path);
+            file = file.Replace(".", ",");
+            File.WriteAllText(path, file);
+        }
+    }
+}
---
+++ Zadanie1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using ConsoleTables;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var separator = ' ';
+            var samples = @"C:\Users\ja\source\repos\SSI\ConsoleApp3\wartosci.txt";
+            var values = @"C:\Users\ja\source\repos\SSI\ConsoleApp3\atrybuty.txt";
+            var decisionSystem = new DecisionSystem();
+            var sampleBase = new SampleBaseDto
+            {
+                Samples = decisionSystem.ReadSamples(samples, separator),
+                AttrNames = decisionSystem.ReadAttributeNames(values, separator),
+                IfAttrSym = decisionSystem.CheckIfAttrSym(values, separator)
+            };
+
+            var table = new ConsoleTable(sampleBase.AttrNames.ToArray());
+
+            foreach (var sample in sampleBase.Samples)
+            {
+                table.AddRow(sample.ToArray());
+            }
+
+            table.Write();
+            Console.WriteLine("Samples[0][2] = " + sampleBase.Samples[0][2]);
+            Console.WriteLine("IfAttrSym[1] = " + sampleBase.IfAttrSym[1]);
+            Console.WriteLine("AttrNames[1] = " + sampleBase.AttrNames[1]);
+            Console.ReadKey();
+        }
+    }
+}
