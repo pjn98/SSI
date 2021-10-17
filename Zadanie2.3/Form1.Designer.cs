9fe5f86cc59b42b94e94668e695e2cc9f2f146bc
pbartko <pbartko@netland.com.pl>
2021-10-17 14:52:47 +0200

    chart

---
+++ Zadanie2.3/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿
+namespace Zadanie2._3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.Wykres = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.Wykres)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Wykres
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.Wykres.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.Wykres.Legends.Add(legend1);
+            this.Wykres.Location = new System.Drawing.Point(12, 12);
+            this.Wykres.Name = "Wykres";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.Wykres.Series.Add(series1);
+            this.Wykres.Size = new System.Drawing.Size(760, 469);
+            this.Wykres.TabIndex = 0;
+            this.Wykres.Text = "Wykres";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 488);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(760, 42);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Generuj wykres";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Wykres);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.Wykres)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart Wykres;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Zadanie2.3/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace Zadanie2._3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Wykres.Series.Clear();
+            var series1 = Wykres.Series.Add("łamane");
+            series1.ChartType = SeriesChartType.Spline;
+            var series2 = Wykres.Series.Add("punkty");
+            series2.ChartType = SeriesChartType.Point;
+            var series3 = Wykres.Series.Add("sinus");
+            series3.ChartType = SeriesChartType.Spline;
+
+            var series1XPoints = new List<double> {-2, -1.7, -1.3, -0.8, 0, 0.8, 1.3, 1.7, 2, 1.7, 1.3, 0.8, 0, -0.8, -1.3, -1.7, -2};
+            var series1YPoints = new List<double> {0, 0.8, 1.5, 1.8, 2, 1.8, 1.5, 0.8, 0, -0.8, -1.5, -1.8, -2, -1.8, -1.5, -0.8, 0 };
+
+            var series2XPoints = new List<double> {-1, 0, 1 };
+            var series2YPoints = new List<double> {1, 0 , 1 };
+
+            var series3XPoints = new List<double> {-1, 0, 1 };
+            var series3YPoints = new List<double> {0, -1, 0 };
+
+            for (var i = 0; i < series1XPoints.Count; i++)
+                series1.Points.AddXY(series1XPoints[i], series1YPoints[i]);
+
+            for (var i = 0; i < series2XPoints.Count; i++)
+                series2.Points.AddXY(series2XPoints[i], series2YPoints[i]);
+
+            for (var i = 0; i < series3XPoints.Count; i++)
+                series3.Points.AddXY(series3XPoints[i], series3YPoints[i]);
+
+        }
+    }
+}
